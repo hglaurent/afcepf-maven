de8d68434b8014c0fdf504aaccc4c3756e73f9c3
LaurentGH <grimaldi.laurentgh@gmail.com>
2019-04-29 14:46:16 +0200

    initial commit

---
+++ src/main/java/com/formation/Calculateur.java
@@ -0,0 +1,14 @@
+package com.formation;
+
+public class Calculateur 
+{
+	public int add(int a, int b)
+	{
+		return a + b;
+	}
+	
+	public int mult(int a, int b)
+	{
+		return a * b;
+	}
+}
---
+++ src/test/java/com/formation/CalculateurTest.java
@@ -0,0 +1,29 @@
+package com.formation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class CalculateurTest 
+{
+	private static Calculateur calcul = null;
+	
+	@BeforeAll
+	public static void init()
+	{
+		calcul = new Calculateur();
+	}
+	
+	@Test
+	public void addTest()
+	{
+		assertEquals(5, calcul.add(2, 3), "Add not ok !");
+	}
+
+	@Test
+	public void multTest()
+	{
+		assertEquals(6, calcul.mult(2, 3), "Multiply not ok !");
+	}
+}
